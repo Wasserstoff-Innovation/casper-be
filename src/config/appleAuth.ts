3794e563a27de680dd766f2e7575abcb82f2abde
dabloo-wstf <dabloo.yadav@thewasserstoff.com>
2025-10-08 18:28:30 +0530

    minor change

--- src/config/appleAuth.ts
+++ src/config/appleAuth.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { envConfigs } from './envConfig';
 
-const privateKey = fs.readFileSync(path.join(__dirname, '../config/AuthKey_PA2257KZH7.p8')).toString();
+const privateKey = "";//fs.readFileSync(path.join(__dirname, '../config/AuthKey_PA2257KZH7.p8')).toString();
 const config: AppleAuthConfig = {
   client_id: envConfigs.appleClientId,
   team_id: envConfigs.appleTeamId,
